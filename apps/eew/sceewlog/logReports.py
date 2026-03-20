eb50a48f4109e2d1ebfa76a1324a4a7550bc5c4d
Thomas Planès <thomas.planes@sed.ethz.ch>
2026-03-20 15:42:32 +0100

    cleanup before merge

--- apps/eew/sceewlog/logReports.py
+++ apps/eew/sceewlog/logReports.py
@@ -1,13 +1,34 @@
-# New report (centroid) generation functions for sceewlog
+"""
+Copyright (C) by ETHZ/SED
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+Module Description
+------------------
+The logReport module provides helper functions for the new sceewlog 
+report generation (03.2026). The new report format includes the 
+EEW preferred solution, point-source updates with epicenter, 
+and finite-source updates with finder centroid.
+
+Author: Thomas Planès, based on previous code by Yannik Behr and Fred Massin
+"""
 
 import os
 
 
 def generateReport(event, report_headers):
     """
-    Generate a report for an event, including the preferred solution, point-source and finite-source updates.
+    Generate a report for an event, including the preferred solution,
+    point-source updates, and finite-source updates.
     """
-
     header_point_src, header_finite_src = report_headers
     updates = sorted(event['updates'].keys())
     u_pref = updates[-1] # get the latest update as the preferred solution
@@ -18,18 +39,19 @@ def generateReport(event, report_headers):
     report_finite_src = ""
     if len(finite_src_updates) > 0:
         report_finite_src = header_finite_src + "\n".join(finite_src_updates) + "\n"
-
-    report_pref = getFormattedPrefSolution(org_pref)
+    report_pref = getFormattedPrefOrigin(org_pref)
     report = "\n\n".join([report_pref, report_point_src, report_finite_src])
     
     event['diff'] = event['updates'][updates[0]]['difftopref'] # modified, first solution should be fastest by def
     event['type'] = org_pref['type']
     event['magnitude'] = org_pref['magnitude']
     event['report'] = report
+    return None
+
 
 def getUpdatesSolutions(event, updates, org_pref):
     """
-    Build the point-source and finite-source solutions updates/lines for the report.
+    Compile the point-source and finite-source solutions updates for the report.
     Save the max update magnitude for downstream email threshold checking 
     (done here to maintain legacy behavior).
     """
@@ -41,18 +63,100 @@ def getUpdatesSolutions(event, updates, org_pref):
         org_curr = event['updates'][update]
         if org_curr['eew'] is True:
             i_alert += 1
-        difftime = org_curr['tsobject'] - org_pref['tsobject']
+        difftime = org_curr['tsobject'] - org_pref['tsobject']  # diff between curr. origin and pref. origin creation time
         org_curr['difftopref'] = difftime.length() + org_pref['diff']
         max_mag = max(max_mag, org_curr['magnitude'])
-        format_params_point_src, format_params_finite_src = getFormattedUpdate(org_curr, i_update, i_alert)
+
+        simple_author = org_curr['author'].split('@')[0]  # Returns name before '@' if it exists, otherwise returns the whole string
+        format_params_point_src = getFormatParamsPointSrc(org_curr, i_update, i_alert, simple_author)
         point_src_updates.append("|".join(format_params_point_src))
-        if format_params_finite_src is not None:
+
+        format_params_finite_src = None
+        if org_curr['centroid_lat'] is not None and org_curr['centroid_lon'] is not None:
+            format_params_finite_src = getFormatParamsFiniteSource(org_curr, i_update, simple_author)
             finite_src_updates.append("|".join(format_params_finite_src))
+    
     event['max_mag'] = max_mag
     return point_src_updates, finite_src_updates
 
 
-def storeRep(evID, report_directory, report):
+def getFormattedPrefOrigin(org_pref):
+    """
+    Extract and format the preferred origin data for the report.
+    """
+    pref_params = (
+            "EEW reference solution:\n",
+            f"Time:   {org_pref['ot'].replace('Z', ' UTC')}",
+            f"Lat:    {org_pref['lat']:.3f}",
+            f"Lon:    {org_pref['lon']:.3f}",
+            f"Depth:  {org_pref['depth']:.1f}",
+            f"Mag:    {org_pref['magnitude']:.2f} {org_pref['type']}",
+            f"Author: {org_pref['author']}"
+    )
+    return "\n".join(pref_params)
+
+
+def getFormatParamsPointSrc(org, i_update, i_alert, simple_author):
+        """
+        Extract and format the point-source solution data for the current update.
+        """
+        format_params_point_src = (
+            f"{i_update:>3d}",
+            f"{org['difftopref']:>6.2f}",
+            f"{org['type']:>4s}",
+            f"{org['magnitude']:>5.2f}", 
+            f"{org['lat']:>7.3f}", 
+            f"{org['lon']:>8.3f}", 
+            f"{org['depth']:>6.1f}", 
+            f"{org['ot'][11:22]:>12s}", 
+            f"{org['likelihood']:5.2f}" if 'likelihood' in org else " " * 5,
+            f"{org['nstorg']:>3d}",
+            f"{org['nstmag']:>3s}", 
+            f" {org['ts'][11:22]:s}", 
+            f" {simple_author[:9]:<9s}", 
+            f"{org['diff']:>7.2f}",
+            f"{i_alert:>4d}" if org['eew'] else " " * 4
+        )
+        return format_params_point_src
+
+
+def getFormatParamsFiniteSource(org, i_update, simple_author):
+    """
+    Extract and format the finite-source solution data for the current update.
+    """
+    format_params_finite_src = (
+        f"{i_update:>3d}",
+        f"{org['difftopref']:6.2f}",
+        f"{org['centroid_lat']:7.3f}", 
+        f"{org['centroid_lon']:8.3f}", 
+        f"{int(org['rupture-strike']):4d}" if 'rupture-strike' in org else " " * 4, 
+        f"{org['rupture-length']:5.1f}" if 'rupture-length' in org else " " * 5, 
+        f" {org['ts'][11:22]:s}", 
+        f" {simple_author[:9]:<9s}"
+    )
+    return format_params_finite_src
+
+
+def createReportHeaders():
+    """
+    Create the headers for the report tables.
+    """
+    point_src = (
+        "Table 1: Point-source solutions\n",
+        "                                                                | #St.  |                               | Alert ",
+        "  #|dt-ref|Type|  Mag|   Lat |    Lon | Depth|  Orig time | Lik | Or| Ma|   Creation | Author   |dt-curr| App",
+        "---------------------------------------------------------------------------------------------------------------\n"
+    )
+    finite_source = (
+        "Table 2: Finite-source solutions\n",
+        "          |   Centroid     |",
+        "  #|dt-ref|   Lat |    Lon | Str| Len |   Creation | Author",
+        "-----------------------------------------------------------\n"
+    )
+    return "\n".join(point_src), "\n".join(finite_source)
+
+
+def saveReportToDisk(evID, report_directory, report):
         """
         Store the generated report on disk.
         """
@@ -62,91 +166,8 @@ def storeRep(evID, report_directory, report):
                                 f"{evID.replace('/', '_')}_report.txt"), 'w') as f:
             f.writelines(report)
 
-def getFormattedPrefSolution(ed_pref):
-    """
-    Extract and format the preferred solution data for the report.
-    """
-    pref_params = (
-            "EEW reference solution:\n",
-            f"Time:   {ed_pref['ot'].replace('Z', ' UTC')}",
-            f"Lat:    {ed_pref['lat']:.3f}",
-            f"Lon:    {ed_pref['lon']:.3f}",
-            f"Depth:  {ed_pref['depth']:.1f}",
-            f"Mag:    {ed_pref['magnitude']:.2f} {ed_pref['type']}",
-            f"Author: {ed_pref['author']}"
+
+if __name__ == "__main__":
+    raise SystemExit(
+        "This module provides helper functions for sceewlog and is not meant to be run directly."
     )
-    return "\n".join(pref_params)
-
-def getFormattedUpdate(ed, update_index, alert_index):
-        """
-        Extract and format individual update data for the report.
-        """
-        simple_author = ed['author']
-        author_split_index = simple_author.find("@")
-        if author_split_index != -1:
-            simple_author = simple_author[:author_split_index]
-
-        format_params_point_src = getFormatParamsPointSrc(ed, update_index, alert_index, simple_author)
-
-        format_params_finite_src = None
-        if ed['centroid_lat'] is not None and ed['centroid_lon'] is not None:
-            format_params_finite_src = getFormatParamsFiniteSource(ed, update_index, simple_author)
-        return format_params_point_src, format_params_finite_src
-
-def getFormatParamsPointSrc(ed, update_index, alert_index, simple_author):
-        """
-        Extract and format the point-source solution data for the current update.
-        """
-        format_params_point_src = (
-            f"{update_index:>3d}",
-            f"{ed['difftopref']:>6.2f}",
-            f"{ed['type']:>4s}",
-            f"{ed['magnitude']:>5.2f}", 
-            f"{ed['lat']:>7.3f}", 
-            f"{ed['lon']:>8.3f}", 
-            f"{ed['depth']:>6.1f}", 
-            f"{ed['ot'][11:22]:>12s}", 
-            f"{ed['likelihood']:5.2f}" if 'likelihood' in ed else " " * 5,
-            f"{ed['nstorg']:>3d}",
-            f"{ed['nstmag']:>3s}", 
-            f" {ed['ts'][11:22]:s}", 
-            f" {simple_author[:9]:<9s}", 
-            f"{ed['diff']:>7.2f}",
-            f"{alert_index:>4d}" if ed['eew'] else " " * 4
-        )
-        return format_params_point_src
-            
-def getFormatParamsFiniteSource(ed, update_index, simple_author):
-    """
-    Extract and format the finite-source solution data for the current update.
-    """
-    format_params_finite_src = (
-        f"{update_index:>3d}",
-        f"{ed['difftopref']:6.2f}",
-        f"{ed['centroid_lat']:7.3f}", 
-        f"{ed['centroid_lon']:8.3f}", 
-        f"{int(ed['rupture-strike']):4d}" if 'rupture-strike' in ed else " " * 4, 
-        f"{ed['rupture-length']:5.1f}" if 'rupture-length' in ed else " " * 5, 
-        f" {ed['ts'][11:22]:s}", 
-        f" {simple_author[:9]:<9s}"
-    )
-    return format_params_finite_src
-
-
-def createReportHeaders():
-        """
-        Create the headers for the report tables.
-        """
-        point_src = (
-            "Table 1: Point-source solutions\n",
-            "                                                                | #St.  |                               | Alert ",
-            "  #|dt-ref|Type|  Mag|   Lat |    Lon | Depth|  Orig time | Lik | Or| Ma|   Creation | Author   |dt-curr| App",
-            "---------------------------------------------------------------------------------------------------------------\n"
-        )
-        finite_source = (
-            "Table 2: Finite-source solutions\n",
-            "          |   Centroid     |",
-            "  #|dt-ref|   Lat |    Lon | Str| Len |   Creation | Author",
-            "-----------------------------------------------------------\n"
-        )
-        return "\n".join(point_src), "\n".join(finite_source)
--- apps/eew/sceewlog/sceewlog.py
+++ apps/eew/sceewlog/sceewlog.py
@@ -48,9 +48,9 @@ class Listener(seiscomp.client.Application):
         self.event_dict = {}
         self.origin_lookup = {}
         self.event_lookup = {}
-        self.centroid_lookup = {} # creation time <-> centroidID table to fetch centroid origin from epicenter origin (same creation time in finder) 
+        self.centroid_lookup = {}  # creation time <-> centroidID table to fetch centroid origin from epicenter origin (same creation time in finder) 
         self.received_comments = []
-        self.latest_event = seiscomp.core.Time() # defaults to 01.01.1970
+        self.latest_event = seiscomp.core.Time()  # defaults to 01.01.1970
         # report settings
         self.storeReport = False
         self.ei = seiscomp.system.Environment.Instance()
@@ -625,7 +625,7 @@ class Listener(seiscomp.client.Application):
                 self.sendMail(evDict, evID)            
  
             if self.storeReport:
-                logReports.storeRep(evID, self.report_directory, evDict['report'])
+                logReports.saveReportToDisk(evID, self.report_directory, evDict['report'])
             seiscomp.logging.info("\n" + evDict['report'])
             evDict['published'] = True
             timer.reset()
@@ -1350,22 +1350,23 @@ class Listener(seiscomp.client.Application):
                                     seiscomp.logging.debug( "Current update has less or equal number of stations: %s than the previous reported update: %s. No further evaluation." % ( str(nstmag), str( lastEvtSent['nstmag'] ) ) )
                                     return
                             
-            #finally, checking if the mag, lat, lon and depth significantly changed
-            if len( lastEvtSent ) > 0 :
-                latDiffAbs = abs(lastEvtSent['lat'] - evt['lat'])
-                lonDiffAbs = abs(lastEvtSent['lon'] - evt['lon'])
-                depthDiffAbs = abs(lastEvtSent['depth'] - evt['depth'])
-                magDiffAbs = abs(lastEvtSent['magnitude'] - evt['magnitude'])
+        #finally, checking if the mag, lat, lon and depth significantly changed
+        #edit tho: removed from magAssociate scope to always be checked
+        if len( lastEvtSent ) > 0 :
+            latDiffAbs = abs(lastEvtSent['lat'] - evt['lat'])
+            lonDiffAbs = abs(lastEvtSent['lon'] - evt['lon'])
+            depthDiffAbs = abs(lastEvtSent['depth'] - evt['depth'])
+            magDiffAbs = abs(lastEvtSent['magnitude'] - evt['magnitude'])
+            
+            #evaluating the delta values
+            if latDiffAbs >= 0.5 or lonDiffAbs >= 0.5 or depthDiffAbs >= 20 or magDiffAbs >=0.2:
+                seiscomp.logging.debug("Significant change -> deltaLat: %s, deltaLon: %s, deltaMag: %s, deltaDepth: %s" % \
+                (latDiffAbs, lonDiffAbs, magDiffAbs, depthDiffAbs) )
                 
-                #evaluating the delta values
-                if latDiffAbs >= 0.5 or lonDiffAbs >= 0.5 or depthDiffAbs >= 20 or magDiffAbs >=0.2:
-                    seiscomp.logging.debug("Significant change -> deltaLat: %s, deltaLon: %s, deltaMag: %s, deltaDepth: %s" % \
-                    (latDiffAbs, lonDiffAbs, magDiffAbs, depthDiffAbs) )
-                    
-                else:
-                    seiscomp.logging.debug("Not significant change -> deltaLat: %s, deltaLon: %s, deltaMag: %s, deltaDepth: %s" % \
-                    (latDiffAbs, lonDiffAbs, magDiffAbs, depthDiffAbs) )
-                    return
+            else:
+                seiscomp.logging.debug("Not significant change -> deltaLat: %s, deltaLon: %s, deltaMag: %s, deltaDepth: %s" % \
+                (latDiffAbs, lonDiffAbs, magDiffAbs, depthDiffAbs) )
+                return
                         
         #first Checking if origin location is within a closed polygon
         #there might be more than one polygon but once
